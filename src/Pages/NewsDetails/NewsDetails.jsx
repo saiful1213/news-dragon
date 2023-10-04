91cefaf173cd4216103b585b42ebb8a67f51e90d
saiful-islam <md2215566@gmail.com>
2023-10-05 01:59:23 +0600

    news details update

--- src/Pages/NewsDetails/NewsDetails.jsx
+++ src/Pages/NewsDetails/NewsDetails.jsx
@@ -2,16 +2,34 @@ import { useParams } from "react-router-dom";
 import Header from "../shared/Header/Header";
 import Navbar from "../shared/Navbar/Navbar";
 import RightSideNav from "../shared/RightSideNav/RightSideNav";
+import { useEffect, useState } from "react";
+import { AiOutlineArrowLeft } from "react-icons/ai"
 
 const NewsDetails = () => {
    const { id } = useParams();
+   const [data, setData] = useState([]);
+
+   useEffect(()=>{
+      fetch('/news.json')
+      .then(res => res.json())
+      .then(data => setData(data))
+   },[])
+
+   const selectedCards = data.find(obj => obj._id === id);
+
+   const {image_url, title, details} = selectedCards || {};
+
    return (
       <div>
          <Header></Header>
          <Navbar></Navbar>
-         <div className="grid md:grid-cols-4">
-            <div className="col-span-3">
-               <h1>News Details of {id}</h1>
+         <div className="grid md:grid-cols-4 gap-3">
+            <div className="col-span-3 p-7">
+               <h1 className="text-[#403F3F] font-semibold text-xl mb-12">Dragon News</h1>
+               <img src={image_url} className=  "w-full"/>
+               <h1 className="font-bold text-2xl text-[#403F3F] mt-5 mb-2">{title}</h1>
+               <p className="text-[#706F6F]">{details}</p>
+               <button className="bg-[#D72050] btn text-white mt-8"><AiOutlineArrowLeft size={'1.5rem'}/>All news in this category</button>
             </div>
             <div>
                <RightSideNav></RightSideNav>
--- src/Pages/shared/Navbar/Navbar.jsx
+++ src/Pages/shared/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 import defalutUserImg from "../../../assets/user.png"
 import { useContext } from "react";
 import { AuthContext } from "../../../Provider/AuthProvider";
+import { RiMenuLine } from "react-icons/ri";
 
 
 const Navbar = () => {
@@ -18,15 +19,15 @@ const Navbar = () => {
       <div className="navbar bg-base-100">
          <div className="navbar-start">
             <div className="dropdown">
-               <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+               <label tabIndex={0} className="btn btn-ghost md:hidden">
+                  <RiMenuLine className="text-2xl"/>
                </label>
                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                   {Navlinks}
                </ul>
             </div>
          </div>
-         <div className="navbar-center hidden lg:flex">
+         <div className="navbar-center hidden md:flex">
             <ul className="menu menu-horizontal px-1">
                {Navlinks}
             </ul>
